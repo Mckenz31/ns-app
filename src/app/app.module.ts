6670a1d9af8626c274b6b4369a818a63cd64069f
Mckenz31 <mckenz318@gmail.com>
2020-06-03 20:35:05 +0530

    first commit:(52)Building our first own component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,7 @@
 import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
 import { NativeScriptModule } from "nativescript-angular/nativescript.module";
-
-import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
-import { ItemsComponent } from "./item/items.component";
-import { ItemDetailComponent } from "./item/item-detail.component";
+import { CurrentChallengesComponent} from "./challenges/current-challenges/current-challenges.component";
 
 // Uncomment and add to NgModule imports if you need to use two-way binding
 // import { NativeScriptFormsModule } from "nativescript-angular/forms";
@@ -18,12 +15,10 @@ import { ItemDetailComponent } from "./item/item-detail.component";
     ],
     imports: [
         NativeScriptModule,
-        AppRoutingModule
     ],
     declarations: [
         AppComponent,
-        ItemsComponent,
-        ItemDetailComponent
+        CurrentChallengesComponent
     ],
     providers: [],
     schemas: [
---
+++ src/app/challenges/current-challenges/current-challenges.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+
+@Component ({
+    selector: 'ns-current-challenges',
+    templateUrl: './current-challenges.component.html',
+    styleUrls: ['./current-challenges.component.css'],
+    moduleId: module.id
+})
+
+export class CurrentChallengesComponent {}
